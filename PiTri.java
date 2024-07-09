f0608138147542ec5a7aca6fa93c36c4af86685e
Nikolay <8790@gmail.com>
2024-07-09 10:04:06 +0300

    six commit

--- PiTri.java
+++ PiTri.java
@@ -30,6 +30,10 @@ public class PiTri {
             int a = Integer.parseInt(args[0]);
             int b = Integer.parseInt(args[1]);
             int c = Integer.parseInt(args[2]);
+              int a = Integer.parseInt(args[0]);
+            int b = Integer.parseInt(args[1]);
+            int c = Integer.parseInt(args[2]);
+//
 //            int a = Integer.parseInt(args[0]);
 //            int b = Integer.parseInt(args[1]);
 //            int c = Integer.parseInt(args[2]);
@@ -40,6 +44,12 @@ public class PiTri {
             if(checkTriple(a, b, c)){
                 System.out.println(a + ", "+ b + ", "+ c + " — это настоящая пифагорова тройка!");
             } else {
+                System.out.println("Эти числа не являются пифагоровой тройкой");
+                System.out.println("Эти числа не являются пифагоровой тройкой");
+                System.out.println("Эти числа не являются пифагоровой тройкой");
+                System.out.println("Эти числа не являются пифагоровой тройкой");
+                System.out.println("Эти числа не являются пифагоровой тройкой");
+
                 System.out.println("Эти числа не являются пифагоровой тройкой");
                 System.out.println("Эти числа не являются пифагоровой тройкой");
                 System.out.println("Эти числа не являются пифагоровой тройкой");
